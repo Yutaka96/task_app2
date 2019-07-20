0c1b10ca24231cedb4ee8aad7aeb7d6b847280e0
小松崎豊 <yutaka96.koma@gmail.com>
2019-07-20 22:16:11 +0900

    add update, edit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all.order(:id)
+  end
+  def update
+      @task = Task.find(params[:id])
+      if @task.update(task_params)
+        redirect_to root_path
+      else
+        render :edit
+    end
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+  @task = Task.new(task_params)
+  if @task.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+  def task_params
+    params.require(:task).permit(:name,:content)
+  end
+end
